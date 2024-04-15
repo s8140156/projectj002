ec0dff05dfaa28ae44605b1554b18507e4dfd89d
s8140156 <s8140156@gmail.com>
2024-04-15 17:56:36 +0800

    補上jquery函式(如果不放彈跳視窗不動)及modal格式調整測試中

--- modal.php
+++ modal.php
@@ -1,10 +1,13 @@
 <h3>新增行事曆</h3>
 <hr>
 <form action="./api/add.php" method="post">
-    <div style="display:flex;margin:auto;text-align:center">
-        <div>標題</div>
+    <div style="display:flex;flex-wrap:wrap;margin:auto;margin-top:10px;text-align:center">
+        <div>日期:</div>
         <div><input type="date" name="date" id=""></div>
+        <div>標題:</div>
         <div><input type="text" name="title" id=""></div>
+        <div>活動事項:</div>
+        <div><textarea name="texxt" id="" cols="30" rows="10"></textarea></div>
     </div>
     <div>
 		<input type="submit" value="新增">
